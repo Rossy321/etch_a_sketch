f06acb0bb0d7921b730adc59b3b5b88c3f9f9318
Rossy321 <rossritchie.09@gmail.com>
2025-05-13 19:47:29 +0100

    Add reset button with prompt to create new grid
    - Create button in the DOM and append to buttonContainer
    - On click, prompts user for number of squares per side
    - Clears existing grid and generates a new one based on input

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,12 +1,31 @@
-const container = document.getElementById("container");
-container.style.border = "2px solid black";
+
+const buttonContainer = document.getElementById("buttonContainer");
+const gridContainer = document.getElementById("gridContainer");
+
+
+const resetButton = document.createElement("button");
+buttonContainer.appendChild(resetButton);
+resetButton.textContent = "Reset Grid";
+resetButton.id = "resetButton";
+
+
+
+
+
+
+gridContainer.style.border = "2px solid black";
+
+
+
 
 function createGrid (rowCount, columnCount) {
 for (let x = 0; x < rowCount; x++) {
 for (let y = 0; y < columnCount; y++) {
    const gridDiv = document.createElement("div");
    gridDiv.classList.add("grid-point");
-   container.appendChild(gridDiv);
+   gridContainer.appendChild(gridDiv);
+   gridDiv.style.width = 960/rowCount + "px";
+   gridDiv.style.height = 960/columnCount + "px";
    gridDiv.addEventListener("mouseover", () => {
    gridDiv.style.backgroundColor = "red";
   });
@@ -17,5 +36,9 @@ for (let y = 0; y < columnCount; y++) {
 createGrid(16, 16);
 
 
-
-//gridDiv.textContent = ".";
+resetButton.addEventListener("click", () => { 
+   let squares = prompt("Number of squares per side for new grid?");
+   let squaresNo = Number(squares);
+   gridContainer.innerHTML = "";
+   createGrid(squaresNo, squaresNo);
+   });
